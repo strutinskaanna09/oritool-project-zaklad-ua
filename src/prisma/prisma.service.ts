61e65eea758c767af56a4c64f3ab3e1471356dc2
strutinskaanna09 <strutinska.anna09@gmail.com>
2026-02-16 16:28:58 +0200

    Create Prisma service file

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,14 +1,11 @@
-import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
-  async onModuleInit() {
-    await this.$connect();
+export class PrismaService extends PrismaClient {
+  constructor() {
+    super();
   }
 
-  async onModuleDestroy() {
-    await this.$disconnect();
-  }
+  // Add any custom methods or logic for connection management here
 }
-
